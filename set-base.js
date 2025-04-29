303e3166afbf47443aea077e9ab8275ea34f5918
R Watts <rw360@student.le.ac.uk>
2025-04-29 21:27:42 +0100

    Added configurations for automatic modification of the base URL.

---
+++ set-base.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const fs = require('fs');
+const path = require('path');
+
+const headPath = path.join(__dirname, 'templates', 'partials', 'head.ejs');
+const content = fs.readFileSync(headPath, 'utf8');
+
+let baseurl = process.env['BASEURL'] != undefined ? process.env['BASEURL']: '...';
+
+const updatedContent = content.replace(
+    /<base\s+href="[^"]*"\s* \/>/,
+    `<base href="${baseurl}" />`
+)
+
+fs.writeFileSync(headPath, updatedContent);
