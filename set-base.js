a4e9f6d5e2887ab6569b4236afa8969dbf183068
R Watts <rw360@student.le.ac.uk>
2025-04-30 11:08:19 +0100

    Removed need for node_modules folder in release.

--- set-base.js
+++ set-base.js
@@ -6,7 +6,7 @@ const path = require('path');
 const headPath = path.join(__dirname, 'templates', 'partials', 'head.ejs');
 const content = fs.readFileSync(headPath, 'utf8');
 
-let baseurl = process.env['BASEURL'] != undefined ? process.env['BASEURL']: '...';
+let baseurl = process.env['BASEURL'] != undefined ? process.env['BASEURL']: 'http://localhost:3000/';
 
 const updatedContent = content.replace(
     /<base\s+href="[^"]*"\s* \/>/,
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,9 @@
-// Import the Bootstrap bundle
-//
-// This includes Popper and all of Bootstrap's JS plugins.
-
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
 //
 // Place any custom JS here
 //
 
 const returnToTopBtn = document.getElementById("returnToTop");
+
 document.onscroll = () => {
   if (document.scrollingElement.scrollTop > 0){
     returnToTopBtn.classList.remove("d-none");
@@ -17,6 +11,8 @@ document.onscroll = () => {
     returnToTopBtn.classList.add("d-none");
   }
 };
+
+returnToTopBtn.onclick = () => document.scrollingElement.scrollTo(0, 0);
 
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
