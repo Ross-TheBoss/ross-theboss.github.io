3660eca9dedb241b4e90cf591fec0276dce544bf
R Watts <rw360@student.le.ac.uk>
2025-05-04 17:24:20 +0100

    Refactored the share modal and added google analytics.

--- static/js/share.js
+++ static/js/share.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     shareLink.href = document.location;
     shareLink.textContent = document.location;
 
+    const facebookLink = document.getElementById("facebook-link");
+    facebookLink.href = `https://www.facebook.com/sharer/sharer.php?u=${encodeURI(document.location)}&amp;src=sdkpreparse`;
+
+    const twitterLink = document.getElementById("twitter-link");
+    twitterLink.href = `https://twitter.com/intent/tweet?url=${encodeURI(document.location)}`;
+
     const copyShareLinkBtn = document.getElementById("copy-share-link");
     copyShareLinkBtn.onclick = () => {
         navigator.clipboard.writeText(document.location);
